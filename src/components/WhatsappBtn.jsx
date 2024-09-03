9d18e53477477137314b08572273af118a8f91b5
Abhinav-378 <abhichoukse05@gmail.com>
2024-09-03 18:49:23 +0530

    wp btn update

--- src/components/WhatsappBtn.jsx
+++ src/components/WhatsappBtn.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import icon from "../assets/wpIcon.svg";
+import icon from "../assets/wpIcon.png";
 function WhatsappBtn() {
   return (
     <>
       <a href="https://api.whatsapp.com/send?phone=919840962950&text=Hello!%20I%27m%20enquring%20about%20Colorglo%20Services%20on%20https%3A%2F%2Fcolorglo.in" target="_blank">
-        <div className="md:w-[56px] md:h-[56px] h-12 w-12 bg-green-400 rounded-full bottom-14 md:bottom-10 right-10 fixed z-50 ">
+        <div className="md:w-[56px] md:h-[56px] h-12 w-12 bottom-14 md:bottom-10 right-10 fixed z-50 ">
           <img src={icon} alt="" />
         </div>
       </a>
